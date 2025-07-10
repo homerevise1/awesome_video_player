fcddf57e2381274ab6b8f2b0e418d8891c128fce
Mit Rajput <mitsinh11@gmail.com>
2025-07-10 12:56:52 +0530

    changed size

--- lib/src/core/better_player.dart
+++ lib/src/core/better_player.dart
@@ -178,14 +178,20 @@ class _BetterPlayerState extends State<BetterPlayer>
         child: Stack(
           children: [
             controllerProvider,
-            Positioned.fill(
-              child: Opacity(
-                opacity: 0.1,
-                child: Image.network(
-                    'https://homerevise.co.in/assets/img/hr-logo-copy.jpg',
-                height: 80,
-                  width: 80,
-                  fit: BoxFit.cover,
+            Positioned(
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Opacity(
+                  opacity: 0.1,
+                  child: Image.network(
+                      'https://homerevise.co.in/assets/img/hr-logo-copy.jpg',
+                  height: 80,
+                    width: 80,
+                    fit: BoxFit.contain,
+                  ),
                 ),
               ),
             ),
